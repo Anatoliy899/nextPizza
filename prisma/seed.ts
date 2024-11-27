25b2c0e091cd41180156c05a7013c78346f850fe
Anatoly <yashmanov@gmail.com>
2024-11-27 03:28:57 +0300

    add bCrypt

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,34 @@
-async function up() {}
+import { prisma } from './prisma-client';
+import { hashSync } from 'bcrypt';
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: 'User',
+        email: 'user@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'USER',
+      },
+      {
+        fullName: 'Admin',
+        email: 'userAdmin@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'ADMIN',
+      },
+    ],
+  });
+}
 
 async function down() {}
 
-async function main() {}
-async function main2() {}
-async function main3() {}
-async function main4() {}
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (error) {
+    console.error(error);
+  }
+}
