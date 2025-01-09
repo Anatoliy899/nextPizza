cf0cd8cb3697476dc7b5b03f94ab59419eacf305
Anatoly <yashmanov@gmail.com>
2025-01-09 04:23:37 +0300

    Add pizza type in pizza-form

--- components/shared/modals/choose-pizza-form.tsx
+++ components/shared/modals/choose-pizza-form.tsx
@@ -5,7 +5,12 @@ import { Title } from '../title';
 import { Button } from '@/components/ui/button';
 import { PizzaImage } from '../products/pizza-image';
 import { GroupVariants } from '../products/group-variants';
-import { pizzaSizes } from '@/shared/constants/pizza';
+import {
+  PizzaSize,
+  pizzaSizes,
+  PizzaType,
+  pizzaTypes,
+} from '@/shared/constants/pizza';
 
 interface Props {
   imageUrl: string;
@@ -26,17 +31,31 @@ export const ChoosePizzaForm: React.FC<Props> = ({
   onSubmit,
   className,
 }) => {
+  const [size, setSize] = React.useState<PizzaSize>(20);
+  const [type, setType] = React.useState<PizzaType>(1);
+
   const textDetaills = '30 см, традиционное тесто 30';
   const totalPrice = 350;
-  const size = 30;
   return (
     <div className={cn(className, 'flex flex-1')}>
       <PizzaImage imageUrl={imageUrl} size={size} />
       <div className="w-[490px] bg-[#f7f6f5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
-        <p className="text-gray-400">{textDetaills}</p>
+        <p className="text-gray-400">{textDetaills} </p>
 
-        <GroupVariants items={pizzaSizes} />
+        <div className="flex flex-col gap-4">
+          <GroupVariants
+            items={pizzaSizes}
+            value={String(size)}
+            onClick={(value) => setSize(Number(value) as PizzaSize)}
+          />
+
+          <GroupVariants
+            items={pizzaTypes}
+            value={String(type)}
+            onClick={(value) => setType(Number(value) as PizzaType)}
+          />
+        </div>
 
         <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
           Добавить в корзину за {totalPrice} ₽
